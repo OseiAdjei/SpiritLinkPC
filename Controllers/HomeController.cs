86c4035ccf406ddbbb86fe496121f594b2997ab1
Edward Adjei <wikieddi@icloud.com>
2023-12-13 16:31:01 +0000

    Added Authorizations to Controllers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SpiritLink.Models;
 using System.Diagnostics;
 
 namespace SpiritLink.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
